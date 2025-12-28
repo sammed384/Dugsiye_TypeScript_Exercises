69a3477904a637d74130442facbe882701d9dc84
sammed384 <sammed384@gmail.com>
2025-12-28 10:22:11 +0300

    Exercise 2

---
+++ Exercises 2/README.md
@@ -0,0 +1,3 @@
+# Exercise 2
+
+![Exercises 2](./assets/1.png)
---
+++ Exercises 2/index.ts
@@ -0,0 +1,21 @@
+// 1. 🧪 Typed Array
+
+let names : string[]= ["sam","med"];
+let grades : number[] = [1,2];
+let status1 : boolean []= [true, false];
+
+names.push(20);
+grades.push(true);
+status1.push(20);
+
+// 2. 🔧 Convert to TypeScript
+
+// let products = ["Phone", "Laptop", 99];
+
+let products: string[] = ["Phone", "Laptop", 99];
+
+
+// ### 3. 🎯 Use Tuples for Fixed Values
+
+let location1 : [string, number, number] = ["Paris", 10000000, 500051000];
+
